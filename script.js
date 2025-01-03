bf8e393c7cbbd287fb07866f0b5f17f6ac15860b
F-Myers. <fmy150504@gmail.com>
2025-01-03 09:48:23 +0700

    Add project.html

--- script.js
+++ script.js
@@ -5,18 +5,3 @@ if (form) {
         alert('Thank you for your message!');
     });
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('footer.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to load footer: ' + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            // Masukkan konten footer.html ke elemen footer
-            document.querySelector('footer').innerHTML = data;
-        })
-        .catch(error => console.error(error));
-});
